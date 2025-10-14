8f4d3d92c63a800c24f1e79a4b87409ca98431e3
Paul Cross <me@paulcross.cz>
2025-10-14 22:53:18 +0200

    better dev setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/main.ts'),
       name: 'Slaydover',
-      fileName: (format) => `slaydover.${format}.js`,
+      fileName: (format) => `slaydover.${format}.js`
     },
     rollupOptions: {
       external: ['vue'],
       output: {
         globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
