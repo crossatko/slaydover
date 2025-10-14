b731d601e8eed44f89a2a8c469dbcaab3f666eb0
Paul Cross <me@paulcross.cz>
2025-10-15 00:35:19 +0200

    enable global component registration

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+import type { App } from 'vue'
 import Slaydover from './components/Slaydover.vue'
+
+const install = (app: App) => {
+  app.component('Slaydover', Slaydover)
+}
+
 export type { Slaydover }
-export default Slaydover
+export default Object.assign(Slaydover, { install })
